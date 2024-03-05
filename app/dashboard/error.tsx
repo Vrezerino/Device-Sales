f9704213c9e8de707314d5c7f42ef8b2288488c4
Patrick <parkpatrick1@gmail.com>
2024-03-05 19:27:59 +0200

    edit error page

--- app/dashboard/error.tsx
+++ app/dashboard/error.tsx
@@ -15,7 +15,8 @@ export default function Error({
  
   return (
     <main className="flex h-full flex-col items-center justify-center">
-      <h2 className="text-center">Something went wrong!</h2>
+      <h2 className="text-center text-red-500">Error:</h2>
+      <p>{error.message}</p>
       <button
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
         onClick={
