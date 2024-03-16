39b989bdeef2f1c3b636936945f902f71a62d190
Patrick <parkpatrick1@gmail.com>
2024-03-16 17:51:04 +0200

    Remove redux-persist dependency

--- redux/provider.tsx
+++ redux/provider.tsx
@@ -2,14 +2,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { persistStore } from 'redux-persist';
-//import { PersistGate } from 'redux-persist/integration/react';
 
-//let persistor = persistStore(store);
 export default function ReduxProvider({ children }: { children: React.ReactNode }) {
     return (
-        //<PersistGate loading={null} persistor={persistor}>
-            <Provider store={store}>{children}</Provider>
-        //</PersistGate>
+        <Provider store={store}>{children}</Provider>
     );
 };
