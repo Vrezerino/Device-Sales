d7a6c9407f1db3da8924617df00a7ab762f3ef5c
Patrick <parkpatrick1@gmail.com>
2024-03-28 05:21:13 +0200

    Fix auth import in loginForm

--- app/ui/loginForm.tsx
+++ app/ui/loginForm.tsx
@@ -10,7 +10,7 @@ import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 
 import { useFormState, useFormStatus } from 'react-dom';
-import { authenticate } from '@/app/lib/actions';
+import { authenticate } from '@/app/lib/actions/users';
 
 export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
