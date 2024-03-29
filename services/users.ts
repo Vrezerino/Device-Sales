28de641f04047cadbcb08e03c8dd650b7d0c93f5
Patrick <parkpatrick1@gmail.com>
2024-03-29 23:43:57 +0200

    Edit user server funcs; Edit pipeline

--- services/users.ts
+++ services/users.ts
@@ -1,15 +1,12 @@
 'use server';
 
 import { User } from '@/app/lib/definitions';
-import { clientPromise } from '@/app/lib/mongodb';
-import { MONGODB_NAME } from '@/app/lib/env';
+import { getMongoDb as db } from '@/app/lib/mongodb';
 
 export async function getUser(email: string) {
     try {
-      const client = await clientPromise;
-      const db = client.db(MONGODB_NAME);
   
-      const user = await db.collection('users').findOne({ email });
+      const user = await (await db()).collection('users').findOne({ email });
       return JSON.parse(JSON.stringify(user));
     } catch (e) {
       console.error(e);
@@ -19,10 +16,7 @@ export async function getUser(email: string) {
   
   export async function createUser(user: User) {
     try {
-      const client = await clientPromise;
-      const db = client.db(MONGODB_NAME);
-  
-      await db.collection('users').insertOne(user);
+      await (await db()).collection('users').insertOne(user);
       //return res.status(201).send('Created');
     } catch (e) {
       console.error(e);
